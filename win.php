5ef10dd7e123303afd6af4bc37ab4398976c149d
Gabriel Acevedo <53916521+Gabril44@users.noreply.github.com>
2021-11-15 11:32:50 -0300

    pantalla de ganaste

--- win.php
+++ win.php
@@ -2,4 +2,47 @@
 
 $fname=$_GET['score'];
 
-echo "tu puntaje es ". $fname;
+//echo "tu puntaje es ". $fname;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="confettistyle.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
+    <title>Ganaste!</title>
+</head>
+<body>
+    <form action="index.html">
+        <h1>GANASTE!!! 🎇</h1>
+        <input type="submit" value="volver al inicio" >
+    </form>
+    <script src="confetti.js"></script>
+    <!-- Confetti  JS-->
+    <script>
+
+        // start
+
+        const start = () => {
+            setTimeout(function() {
+                confetti.start()
+            }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+        };
+
+        //  Stop
+
+        const stop = () => {
+            setTimeout(function() {
+                confetti.stop()
+            }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+        };
+
+        start();
+        stop();
+    </script>
+</body>
+</html>
